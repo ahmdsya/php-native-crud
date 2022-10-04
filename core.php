36d8aa53f0b30754e1764be2f9f97e6d11f00c97
Ahmad Syarif <syarifahmad817@gmail.com>
2022-10-04 20:03:35 +0700

    ubah struktur project

--- core.php
+++ core.php
@@ -2,15 +2,15 @@
 
 $page = (isset($_GET['page']) ? $_GET['page'] : false);
 
-require_once 'constants/constants.php';
-require_once 'database/Database.php';
-require_once 'function/Session.php';
+require_once 'app/constants/constants.php';
+require_once 'app/database/Database.php';
+require_once 'app/function/Session.php';
 
-require_once 'function/Test.php';
-require_once 'function/Auth.php';
-require_once 'function/Prodi.php';
-require_once 'function/Mahasiswa.php';
+require_once 'app/function/Test.php';
+require_once 'app/function/Auth.php';
+require_once 'app/function/Prodi.php';
+require_once 'app/function/Mahasiswa.php';
 
-require_once 'actions/auth.php';
-require_once 'actions/prodi.php';
-require_once 'actions/mahasiswa.php';
+require_once 'app/actions/auth.php';
+require_once 'app/actions/prodi.php';
+require_once 'app/actions/mahasiswa.php';
